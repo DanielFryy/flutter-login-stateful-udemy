de67cece1740f645db3bc575c129acb1b87cb9c9
DanielFryy <dgz1606@gmail.com>
2020-01-05 19:31:13 -0500

    Extraction of validations to a mixin class

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import '../mixins/validation_mixin.dart';
 
 class LoginScreen extends StatefulWidget {
   createState() => LoginScreenState();
 }
 
-class LoginScreenState extends State<LoginScreen> {
+class LoginScreenState extends State<LoginScreen> with ValidationMixin {
   final formKey = GlobalKey<FormState>();
 
   String email = '';
@@ -30,11 +31,7 @@ class LoginScreenState extends State<LoginScreen> {
           labelText: 'Email Address',
           hintText: 'you@example.com',
         ),
-        validator: (value) {
-          if (!value.contains('@')) {
-            return 'Please enter a valid email';
-          }
-        },
+        validator: validateEmail,
         onSaved: (value) {
           email = value;
         },
@@ -46,11 +43,7 @@ class LoginScreenState extends State<LoginScreen> {
           labelText: 'Password',
           hintText: 'Password',
         ),
-        validator: (value) {
-          if (value.length < 4) {
-            return 'Password must be at least 4 characters';
-          }
-        },
+        validator: validatePassword,
         onSaved: (value) {
           password = value;
         },
